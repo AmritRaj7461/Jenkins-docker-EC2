dbbb228de6ff6e7ffd2edd335e4716f0f919f830
PalakMishraGit <amritraj7461@gmail.com>
2026-05-12 20:59:09 +0530

    updated app with utf-8 fix

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+
     if (req.url === "/health") {
       res.writeHead(200);
       res.end("OK");
